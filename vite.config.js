c88eec4ab20f6272b4e6c0a4982328a1d18fc0ce
jaynguyen1510 <ngnhatnam1510@gmail.com>
2025-04-15 22:48:47 +0700

    new

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import scrollbar from "tailwind-scrollbar"; // Sử dụng import thay vì require
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import scrollbar from 'tailwind-scrollbar'; // Sử dụng import thay vì require
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        secure: false,
-      },
+    server: {
+        port: 5174,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                secure: false,
+            },
+        },
     },
-  },
-  plugins: [
-    react(),
-    scrollbar, // Thêm plugin scrollbar
-  ],
+    plugins: [
+        react(),
+        scrollbar, // Thêm plugin scrollbar
+    ],
 });
